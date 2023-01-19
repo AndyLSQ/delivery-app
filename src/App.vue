8db45607f309f56e0b16c496405d93406af1fffc
Andy <andrew.eliscu@gmail.com>
2023-01-19 16:32:39 -0600

    Remove placeholder content

--- src/App.vue
+++ src/App.vue
@@ -1,91 +1,76 @@
 <script setup>
-import { RouterLink, RouterView } from "vue-router";
-import HelloWorld from "./components/HelloWorld.vue";
+	import { RouterLink, RouterView, useRoute } from 'vue-router';
+	import { useInterfaceStore } from '@/stores/interface';
+
+	const ui = useInterfaceStore();
+	const route = useRoute();
+
+	// watch: {
+	//    $route(){
+	//       // do something
+	//    };
+	// }
 </script>
 
 <template>
-  <header>
-    <img
-      alt="Vue logo"
-      class="logo"
-      src="@/assets/logo.svg"
-      width="125"
-      height="125"
-    />
+	<header v-bind:class="`${route.name} ${ui.menuClass}`">
+		<div class="wrapper">
+			<button @click="ui.toggleMenu()" class="menu-toggle">Toggle menu</button>
+			<nav class="site-menu">
+				<RouterLink to="/">Home</RouterLink>
+				<RouterLink to="/dashboard">Dashboard</RouterLink>
+				<RouterLink to="/restaurants">Restaurants</RouterLink>
+				<RouterLink to="/form">Form</RouterLink>
+			</nav>
+		</div>
+	</header>
+	<main class="outlet">
+		<RouterView />
+	</main>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+	<footer>Footer</footer>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
+	.site-menu {
+		display: flex;
+		flex-direction: row;
+		flex-wrap: wrap;
+	}
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
+	.site-menu a {
+		padding: 1em;
+	}
 
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
+	.router-link-active {
+		border-bottom: 3px solid red;
+	}
 
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
+	@media (min-width: 500px) {
+		.menu-toggle {
+			display: none;
+		}
+	}
 
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
+	@media (max-width: 500px) {
+		.site-menu {
+			display: none;
+		}
 
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
+		.menu-open .site-menu {
+			display: flex;
+			flex-direction: column;
+			position: fixed;
+			top: 10;
+			left: 0;
+			width: 100%;
+			/*         height: 75%;*/
+			background-color: var(--paper);
+			border: 3px solid purple;
+		}
 
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
+		.menu-closed .site-menu {
+			display: none;
+		}
+	}
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,7 @@
-<script setup>
-import TheWelcome from "../components/TheWelcome.vue";
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+   <div class="Home">
+      <h1 class="loud-voice">This is the landing page</h1>
+   </div>
 </template>
+
+<style></style>
