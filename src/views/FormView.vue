07c5f3cd7e432056cfdb11a796b5f4c77acf72d3
Andy <andrew.eliscu@gmail.com>
2023-01-19 16:34:14 -0600

    New form view

---
+++ src/views/FormView.vue
@@ -0,0 +1,9 @@
+<script setup>
+	import RestaurantForm from '../components/RestaurantForm.vue';
+</script>
+
+<template>
+	<h1 class="loud-voice">Add a restaurant</h1>
+
+	<RestaurantForm />
+</template>
