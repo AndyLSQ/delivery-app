56e60d42d0880367be128be02755d778d0f46395
Andy <andrew.eliscu@gmail.com>
2023-01-19 11:50:21 -0600

    Vite config

--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,16 @@ import { fileURLToPath, URL } from 'node:url';
 
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
-import eslint from '@rollup/plugin-eslint'; //
+import eslint from '@rollup/plugin-eslint';
 
 // https://vitejs.dev/config/
 export default defineConfig({
    plugins: [
       vue(),
       {
-         ...eslint({ include: 'src/**/*.*/js' }),
+         ...eslint({
+            include: 'src/**/*.*/js',
+         }),
          enforce: 'pre',
          apply: 'build',
       },
