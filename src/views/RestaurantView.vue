2dede19137d347687ff5d6d848bacb60132cb0ff
Andy <andrew.eliscu@gmail.com>
2023-01-19 16:33:34 -0600

    New view for restaurant detail

---
+++ src/views/RestaurantView.vue
@@ -0,0 +1,16 @@
+<script setup>
+	import { useRoute } from 'vue-router';
+	import { useRestaurantStore } from '@/stores/restaurants';
+
+	const route = useRoute();
+	const restaurants = useRestaurantStore();
+
+	const restaurant = restaurants.list.find(function (record) {
+		return record.slug == route.params.slug;
+	});
+</script>
+
+<template>
+	<!-- {{ route.params.slug }} -->
+	<h1 class="loud-voice">{{ restaurant.name }}</h1>
+</template>
