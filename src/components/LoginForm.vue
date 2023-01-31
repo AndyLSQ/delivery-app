b0c5eb0a29e0a0252960e257517683509f01eb30
Andy <andrew.eliscu@gmail.com>
2023-01-31 15:27:32 -0600

    Add login form component

---
+++ src/components/LoginForm.vue
@@ -0,0 +1,18 @@
+<script setup>
+	import { computed, ref } from 'vue';
+	import { userService } from '@/services/userService.js';
+
+	// const usernameInput = ref(''); -- moved to store
+	const user = userService();
+</script>
+
+<template>
+	<form action="" @submit.prevent="user.login()">
+		<label for="usernameInputField">Username</label>
+		<input id="usernameInputField" type="text" v-model="user.formInput.username" />
+		<button type="submit">Submit</button>
+	</form>
+	Admin? {{ user.isAdmin }} Current user: {{ user.current }}
+</template>
+
+<style scoped></style>
