dd6c11c5fd117907f83dc091c5234cfd7488d1ba
Andy <andrew.eliscu@gmail.com>
2023-01-30 19:26:58 -0600

    Add search menu

--- src/components/SiteMenu.vue
+++ src/components/SiteMenu.vue
@@ -1,15 +1,19 @@
 <script setup>
-	import { ref } from 'vue';
+	import { ref, computed } from 'vue';
 	import { RouterLink, RouterView, useRoute } from 'vue-router';
 	import CartView from '@/views/CartView.vue';
+	import { useRestaurantStore } from '@/stores/restaurants';
 	import { useInterfaceStore } from '@/stores/interface';
 	import { useLoginStore } from '@/stores/login';
 	import { useCartStore } from '@/stores/cart';
+	// import SvgSpriteComponent from '@/components/icons/SvgSprites.vue';
+	import SvgIcons from '@/components/icons/IconTemplate.vue';
 
 	const ui = useInterfaceStore();
 	const login = useLoginStore();
 	const route = useRoute();
 	const cart = useCartStore();
+	const restaurants = useRestaurantStore();
 
 	const sideMenuOpen = ref(false);
 
@@ -17,20 +21,49 @@
 		sideMenuOpen.value = !sideMenuOpen.value;
 		console.log(sideMenuOpen.value);
 	}
+
+	const searchString = ref('');
+
+	const filtered = computed(function () {
+		return restaurants.list.filter(function (item) {
+			return item.name.toLowerCase().includes(searchString.value.toLowerCase());
+		});
+	});
+
+	function searchClick() {
+		searchString.value = '';
+		route.go(0);
+	}
 </script>
 
 <template>
 	<button @click="ui.toggleMenu()" class="menu-toggle">Toggle menu</button>
 	<nav class="site-menu">
 		<div class="logo">
-			<RouterLink to="/">Food<span>hub</span></RouterLink>
+			<RouterLink class="menu-link" to="/">Food<span>hub</span></RouterLink>
 		</div>
 		<!-- <RouterLink to="/dashboard">Dashboard</RouterLink> -->
-		<RouterLink to="/restaurants">Restaurants</RouterLink>
+		<RouterLink class="menu-link" to="/restaurants">Restaurants</RouterLink>
 		<!-- <RouterLink to="/form">Form</RouterLink> -->
-		<RouterLink to="/cart">Cart {{ cart.quantity }}</RouterLink>
-		<RouterLink to="/account">Account</RouterLink>
-		<button @click="toggleSideMenu">side menu</button>
+		<div>
+			<input id="search" type="text" v-model="searchString" placeholder="Search restaurants" />
+			<!-- <button type="submit">Go</button> -->
+			<!-- <h2>Results for {{ searchString }}</h2> -->
+			<ul v-if="searchString != ''">
+				<li v-if="filtered == ''" class="noResults">No results found in your area</li>
+				<li v-for="item in filtered">
+					<RouterLink :to="`/restaurant/${item.slug}`" @click="searchClick">
+						{{ item.name }} {{ item.slug }}
+					</RouterLink>
+				</li>
+			</ul>
+		</div>
+		<RouterLink class="menu-link" to="/account"> <SvgIcons class="svg-icons" name="user" />Sign In</RouterLink>
+		<div class="menu-link" @click="toggleSideMenu">
+			<SvgIcons class="svg-icons" name="cart" />
+			<div class="qBadge">{{ cart.quantity }}</div>
+			Cart
+		</div>
 	</nav>
 	<button @click="login.toggleLogin()" class="login-toggle">{{ login.loginButton }}</button>
 
