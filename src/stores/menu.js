8ef9fd23e3e05aca8b5db3b1d617565763fa19a5
Andy <andrew.eliscu@gmail.com>
2023-02-01 11:58:31 -0600

    Clean up menu data store

--- src/stores/menu.js
+++ src/stores/menu.js
@@ -9,46 +9,17 @@ import router from '../router';
 export const useMenuStore = defineStore('menu', function () {
 	const list = ref(menuData);
 
-	// console.log(router.useRoute().params);
+	console.log('List:: ', list);
 
-	console.log();
 	const filteredList = computed(function () {
-		// console.log('menu data', menuData);
-		// console.log('list data', list.value);
-
-		// menuData.forEach(function (item) {
-		// 	// console.log('menuData foreach result: ', item.restaurantId);
-		// });
-
-		// list.value.forEach(function (item) {
-		// console.log('list.value foreach result: ', item.restaurantId);
-		// });
-
-		// const menuDataTest = menuData.filter(function (item) {
-		// 	// console.log('menuDataTest item: ', item.restaurantId);
-		// 	return item.restaurantId == 'tbell1';
-		// });
-
-		// console.log('*** menuData test: ', menuDataTest);
-
 		return list.value.filter(function (item) {
 			console.log('listTest slug: ', item.restaurantSlug);
 
 			return item.restaurantSlug == 'chipotle';
 		});
-
-		// console.log('***list test: ', listTest);
-
-		// listTest.forEach(function (item) {
-		// 	console.log('listTest items: ', item.name);
-		// });
-
-		// const test = list.value.filter(function (item) {
-		// 	return item.restaurantId == true;
-		// });
-
-		// console.log('test: ', test);
 	});
+
+	console.log('filteredList:: ', filteredList);
 
 	return {
 		list: list,
