9216ad82ac7a231ccf744c909a6884a375fc977b
Andy <andrew.eliscu@gmail.com>
2023-02-03 11:04:45 -0600

    Cart HTML structure updates

--- src/views/CartView.vue
+++ src/views/CartView.vue
@@ -3,6 +3,7 @@
 	import { useRoute } from 'vue-router';
 	import { useRestaurantStore } from '@/stores/restaurants';
 	import { useCartStore } from '@/stores/cart';
+	import SvgIcons from '@/components/icons/IconTemplate.vue';
 
 	const route = useRoute();
 	const restaurants = useRestaurantStore();
@@ -10,27 +11,45 @@
 </script>
 
 <template>
-	<h1>this is the cart</h1>
+	<div class="empty-cart" v-if="cart.items == ''">
+		<p>Your cart is empty.</p>
+		<picture class="empty-cart-image">
+			<SvgIcons class="svg-icons" name="food" />
+		</picture>
+		<p>Add items to get started</p>
+	</div>
+	<div class="full-cart" v-else>
+		<h1 class="voice1">Cart</h1>
+		<button @click="cart.clearCart()">Clear Cart</button>
+		<ul>
+			<li class="cartItem" v-for="item in cart.items">
+				<h2 class="voice3">{{ item.name }}</h2>
+				<p v-if="item.notes">
+					<em>{{ item.notes }}</em>
+				</p>
+				<!-- <p>${{ item.price }}</p> -->
+				<div>${{ item.subtotal }}</div>
+				<button v-if="item.quantity == 1" @click="cart.remove(item.id)">D</button>
+				<button v-if="item.quantity > 1" @click="cart.minusOne(item)">-</button>
+				{{ item.quantity }}
+				<button @click="cart.plusOne(item)">+</button>
+			</li>
+		</ul>
 
-	<h2>cart list</h2>
-	<ul>
-		<li v-for="item in cart.items">
-			<p>{{ item.name }}</p>
-			<p>Notes: "{{ item.notes }}"</p>
-			<p>${{ item.price }}</p>
-			<p>Qty: {{ item.quantity }}</p>
-			<p>Subtotal: {{ item.subtotal }}</p>
-			<button @click="cart.plusOne(item)">plus</button>
-			<button v-if="item.quantity > 1" @click="cart.minusOne(item)">minus</button>
-			<button @click="cart.remove(item.id)">delete {{ item.id }}</button>
-		</li>
-	</ul>
-	<p>Total quantity: {{ cart.quantity }}</p>
-	<p>Subtotal: ${{ cart.subtotal }}</p>
-	<p>Delivery fee:</p>
-	<p>Tax:</p>
-	<p>Total:</p>
-	<!-- {{ cart.consolList }} -->
+		<!-- <p>Total quantity: {{ cart.quantity }}</p> -->
+		<ul class="cartCalc">
+			<li>Cart Subtotal: ${{ cart.allSubtotal }}</li>
+			<!-- <li>Delivery fee:</li>
+		<li>Tax:</li>
+		<li>Total:</li> -->
+		</ul>
+		<button>Checkout</button>
+		<!-- {{ cart.consolList }} -->
+	</div>
 </template>
 
-<style></style>
+<style scoped>
+	.cartItem {
+		border: 1px solid red;
+	}
+</style>
