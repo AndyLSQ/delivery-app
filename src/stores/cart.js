d9dd861693eade148caab2e92d71caf6681e7977
Andy <andrew.eliscu@gmail.com>
2023-02-03 11:03:25 -0600

    Code cleanup

--- src/stores/cart.js
+++ src/stores/cart.js
@@ -11,22 +11,25 @@ export const useCartStore = defineStore('cart', function () {
 
 	//
 
+	function clearCart() {
+		// let result = window.confirm('pick');
+		// console.log(result);
+
+		// if (confirm('pick one') == true) {
+		// 	console.log('you picked yes');
+		// } else {
+		// 	console.log('you picked no');
+		// }
+		if (window.confirm('Are you sure you want to remove everything from your cart?') == true) {
+			items.value = [];
+		}
+	}
+
 	const count = computed(function () {
 		return items.value.length;
 	});
 
 	// =================== METHOD 2 ========================
-
-	//check if item is in "items" list
-	// function alreadyInList(itemFromMenu) {
-	// 	return items.value.find(function (itemInCart) {
-	// 		console.log(
-	// 			'alreadyInList retuns: ',
-	// 			itemFromMenu.id == itemInCart.id && itemFromMenu.notes == itemInCart.notes,
-	// 		);
-	// 		return itemFromMenu.id == itemInCart.id && itemFromMenu.notes == itemInCart.notes;
-	// 	});
-	// }
 
 	function searchCart(newItem) {
 		// console.log('new item in SEARCHCART: ', newItem);
@@ -56,68 +59,12 @@ export const useCartStore = defineStore('cart', function () {
 			items.value.push(newItem);
 		}
 
-		// items.value.push(newItem);
-		// console.log('!! itemst list now: ', items);
-
-		// const found = alreadyInList(item);
-
-		//if found, quantity +1
-		// if (found) {
-		// 	found.quantity += newQty;
-		// 	console.log('alreadyInList: ', alreadyInList(item, notes));
-		// } else {
-		// 	//if not add it to list with quantitiy 1
-		// 	item.quantity = newQty;
-		// 	items.value.push(item);
-		// 	console.log('item not found. new item list: ');
-		// }
+		// const extPrice = computed(console.log(found.quantity));
 	}
-
-	//  ********
-
-	// function inList(itemFromMenu) {
-	// 	console.log('in list running');
-
-	// 	return items.value.find(function (itemInCart) {
-	// 		console.log('itemFromMenu notes', itemFromMenu.notes);
-	// 		console.log('itemInCart notes', itemInCart.notes);
-	// 		// console.log('itemFromMenu id', itemFromMenu.id);
-	// 		// console.log('itemInCart id', itemInCart.id);
-	// 		return itemFromMenu.notes == itemInCart.notes;
-	// 	});
-	// }
-
-	// function tfTest(newItem) {}
-
-	// function add2(item, singleNote, addQuantity) {
-	// 	item.notes = singleNote;
-	// 	console.log('item with notes added: ', item);
-	// 	const found = inList(item);
-	// 	console.log('found: ', found);
-
-	// 	if (!inList(item)) {
-	// 		console.log('item not found');
-	// 		item.quantity = addQuantity;
-	// 		items.value.push(item);
-	// 	} else {
-	// 		console.log('item found');
-	// 		found.quantity += addQuantity;
-	// 	}
-
-	// if (inList(item)) {
-	// 	console.log('item found');
-	// 	found.quantity += addQuantity;
-	// } else {
-	// 	console.log('item not found');
-	// 	item.quantity = addQuantity;
-	// 	items.value.push(item);
-	// }
-	// }
-	//  ********
 
 	// =======================================================
 	function remove(excludeId) {
-		console.log('remove called', excludeId);
+		// console.log('remove called', excludeId);
 		items.value = items.value.filter(function (item) {
 			return item.id != excludeId;
 		});
@@ -125,30 +72,39 @@ export const useCartStore = defineStore('cart', function () {
 
 	function plusOne(item) {
 		item.quantity += 1;
-		console.log('add this item +1: ', item);
+		// console.log('add this item +1: ', item);
 	}
 
 	function minusOne(item) {
 		item.quantity -= 1;
-		console.log('add this item -1: ', item);
+		// console.log('add this item -1: ', item);
 	}
 
 	// watch(items.value, function () {
 	// 	items.value.forEach(function (item) {
 	// 		let itemSub = item.price * item.quantity;
 	// 		item.subtotal = itemSub;
-	// 		console.log('check subtotal: ', item);
+	// 		console.log('check subtotal: ', item.subtotal);
 	// 	});
 	// });
 
-	const subtotal = computed(function () {
-		let sub = 0;
+	const allSubtotal = computed(function () {
+		let cartSubtotal = 0;
 		items.value.forEach(function (item) {
-			sub += item.price;
+			item.subtotal = item.price * item.quantity;
+			cartSubtotal += item.subtotal;
 		});
-
-		return sub;
+		return cartSubtotal;
 	});
+
+	// const subtotal = computed(function () {
+	// 	let sub = 0;
+	// 	items.value.forEach(function (item) {
+	// 		sub += item.price * item.quantity;
+	// 	});
+
+	// 	return sub;
+	// });
 
 	const quantity = computed(function () {
 		let qty = 0;
@@ -165,10 +121,12 @@ export const useCartStore = defineStore('cart', function () {
 		add: add,
 		// add2,
 		remove: remove,
-		subtotal: subtotal,
+		// subtotal: subtotal,
 		quantity,
 		plusOne,
 		minusOne,
+		allSubtotal,
+		clearCart,
 		// alreadyInList,
 	};
 });
