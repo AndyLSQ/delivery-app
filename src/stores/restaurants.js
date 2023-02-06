cbbb49c65c0063b547397e4292580bbc051084fb
Andy <andrew.eliscu@gmail.com>
2023-02-06 16:44:00 -0600

    Clean up restaurant page

--- src/stores/restaurants.js
+++ src/stores/restaurants.js
@@ -1,7 +1,17 @@
-import { ref, reactive, onBeforeMount, onMounted } from 'vue';
+import { ref, reactive, computed } from 'vue';
 import { defineStore } from 'pinia';
+// import { collection, doc, addDoc, deleteDoc, setDoc } from 'firebase/firestore';
+// import { useFirestore, useCollection } from 'vuefire';
+import { useFavoritesStore } from '@/stores/favorites';
+import SvgIcons from '@/components/icons/IconTemplate.vue';
+
 import restaurantData from '../data/restaurantData.json' assert { type: 'json' };
-// import { useStorage } from '@vueuse/core';
+
+// ==== FB SETUP ====
+// const db = useFirestore();
+
+// Set FB collection of restaurants as reactive data
+// const restaurants = useCollection(collection(db, 'restaurants'));
 
 export const useRestaurantStore = defineStore('restaurants', function () {
 	const list = ref(restaurantData);
@@ -10,14 +20,7 @@ export const useRestaurantStore = defineStore('restaurants', function () {
 		list.push(restaurant);
 	}
 
-	// function getBySlug(slug) {
-	// 	return list.find(function (record) {
-	// 		return record.slug == slug;
-	// 	});
-	// }
-
 	return {
-		// getBySlug: getBySlug,
 		list: list,
 		add: add,
 	};
