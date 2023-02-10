3ee72d5c5ce4a5d34fdec45c45b993a0c0df4c4d
Andy <andrew.eliscu@gmail.com>
2023-02-10 10:53:46 -0600

    Add password show-hide toggle & code cleanup

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -2,23 +2,21 @@
 	import { computed, ref } from 'vue';
 	import { userService } from '@/services/userService.js';
 	const user = userService();
+	const passwordVisibility = ref('password');
 	// const usernameInput = ref(''); -- moved to store
 	// const user = userService();
+
+	function togglePasswordVisibility() {
+		if (passwordVisibility.value == 'password') {
+			passwordVisibility.value = 'text';
+		} else {
+			passwordVisibility.value = 'password';
+		}
+		// console.log('pw value: ', passwordVisibility.value);
+	}
 </script>
 
 <template>
-	<!-- OLD -->
-	<!-- <button @click="userLogin.closeModal()">X</button>
-	<h1 class="loud-voice">Sign In</h1>
-	<button v-if="user.current" @click="user.logout">Log out</button>
-	<form v-if="!user.current" action="" @submit.prevent="user.login()">
-		<label for="usernameInputField">Username</label>
-		<input id="usernameInputField" type="text" v-model="user.formInput.username" />
-		<button type="submit">Submit</button>
-	</form> -->
-	<!-- <p>Admin? {{ user.isAdmin }}</p> -->
-	<!-- <p>Current user: {{ user.current }}</p> -->
-
 	<!-- NEW -->
 	<button type="button" @click="user.signOut()" v-if="user.currentFB">signout</button>
 	<div v-if="user.currentFB">
@@ -35,25 +33,11 @@
 		</div>
 		<div class="form-field">
 			<label for="password1">password</label>
-			<input id="password1" type="text" v-model="user.form.password" />
+			<input id="password1" :type="passwordVisibility" v-model="user.form.password" required />
+			<button type="button" @click="togglePasswordVisibility()">Show/Hide Password</button>
 		</div>
 		<button type="submit">Sign up</button>
 	</form>
-	<!-- 	<form
-		@submit.prevent="user.signUp(user.form.username, user.form.password)"
-		v-if="user.currentFB"
-	>
-		<h2>Sign up</h2>
-		<div class="form-field">
-			<label for="email1">email</label>
-			<input id="email1" type="text" v-model="user.form.username" />
-		</div>
-		<div class="form-field">
-			<label for="password1">password</label>
-			<input id="password1" type="text" v-model="user.form.password" />
-		</div>
-		<button type="submit">Sign up</button>
-	</form> -->
 
 	<form @submit.prevent="user.signIn(user.form.username, user.form.password)" v-if="!user.current">
 		<h2 class="voice1">Sign In</h2>
@@ -63,7 +47,8 @@
 		</div>
 		<div class="form-field">
 			<label for="password2">password</label>
-			<input id="password2" type="text" v-model="user.form.password" />
+			<input id="password2" :type="passwordVisibility" v-model="user.form.password" required />
+			<button type="button" @click="togglePasswordVisibility()">Show/Hide Password</button>
 		</div>
 		<button type="submit">Sign in</button>
 		<div>error message: {{ user.errorMessage }}</div>
