8ff4cb8403097fe7cbed20b3895f30e50c0623c6
Andy <andrew.eliscu@gmail.com>
2023-02-16 11:11:13 -0600

    Add sign out view

---
+++ src/views/SignOutView.vue
@@ -0,0 +1,13 @@
+<script setup>
+	import { userService } from '@/services/userService';
+
+	const user = userService();
+
+	user.signOut();
+</script>
+
+<template>
+	<h1 class="voice1">SIGN OUT</h1>
+</template>
+
+<style scoped></style>
