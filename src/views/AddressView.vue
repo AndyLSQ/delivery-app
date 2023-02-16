8eb2ffd2dd72062fab59e14be158ef8e3a33b564
Andy <andrew.eliscu@gmail.com>
2023-02-16 11:12:41 -0600

    Add account subroute - Address

---
+++ src/views/AddressView.vue
@@ -0,0 +1,26 @@
+<script setup></script>
+
+<template>
+	<h1 class="voice1">Address Book</h1>
+	<p>USE THIS TO ADD MULTIPLE ADDRESSES???</p>
+	<form>
+		<div class="field">
+			<label for="address">Street Address</label>
+			<input id="address" type="text" />
+		</div>
+		<div class="field">
+			<label for="city">City</label>
+			<input id="city" type="text" />
+		</div>
+		<div class="field">
+			<label for="state">State</label>
+			<input id="state" type="text" />
+		</div>
+		<div class="field">
+			<label for="zipCode">Zip Code</label>
+			<input id="zipCode" type="text" />
+		</div>
+	</form>
+</template>
+
+<style scoped></style>
