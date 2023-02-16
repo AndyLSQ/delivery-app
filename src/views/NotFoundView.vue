1190d39274912ea501875fe88eafa7e4140b991a
Andy <andrew.eliscu@gmail.com>
2023-02-16 11:11:48 -0600

    Add 404 view

---
+++ src/views/NotFoundView.vue
@@ -0,0 +1,10 @@
+<script setup>
+	import { RouterLink, RouterView, useRoute } from 'vue-router';
+</script>
+
+<template>
+	<h1 class="voice1">PAGE NOT FOUND</h1>
+	<RouterLink to="/">Take me home</RouterLink>
+</template>
+
+<style scoped></style>
