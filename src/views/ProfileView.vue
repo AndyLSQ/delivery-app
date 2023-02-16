d9fed976047aacd901a8bbd1faf3548a83fb6a9a
Andy <andrew.eliscu@gmail.com>
2023-02-16 11:11:40 -0600

    Add profile view

---
+++ src/views/ProfileView.vue
@@ -0,0 +1,26 @@
+<script setup></script>
+
+<template>
+	<h1 class="voice1">Your Profile</h1>
+
+	<form>
+		<div class="field">
+			<label for="firstName">First Name</label>
+			<input id="firstName" type="text" />
+		</div>
+		<div class="field">
+			<label for="lastName">Last Name</label>
+			<input id="lastName" type="text" />
+		</div>
+		<div class="field">
+			<label for="email">Email</label>
+			<input id="email" type="text" />
+		</div>
+		<div class="field">
+			<label for="phoneNumber">Phone Number</label>
+			<input id="phoneNumber" type="text" />
+		</div>
+	</form>
+</template>
+
+<style scoped></style>
