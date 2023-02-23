02e8b2db0f5268f56a53b4baec8b285105e8b52b
Andy <andrew.eliscu@gmail.com>
2023-02-23 13:09:11 -0600

    New style for full cart

--- src/views/CartView.vue
+++ src/views/CartView.vue
@@ -28,42 +28,67 @@
 		class="full-cart"
 		v-else
 	>
-		<h1 class="voice1">Cart</h1>
-		<button @click="cart.clearCart()">Clear Cart</button>
-		<ul>
+		<div class="header">
+			<h1 class="voice1">Your Cart</h1>
+			<div class="clear-contain">
+				<button @click="cart.clearCart()">Clear Cart</button>
+			</div>
+		</div>
+		<ul class="cart-list">
 			<li
-				class="cartItem"
+				class="cart-item"
 				v-for="item in cart.items"
 			>
-				<h2 class="voice3">{{ item.name }}</h2>
-				<div
-					v-if="item.notes"
-					class="itemNotes"
-				>
-					<em>{{ item.notes }}</em>
+				<!-- <picture>
+					<img
+						:src="item.imageUrl"
+						alt=""
+					/>
+				</picture> -->
+				<div class="cart-item-text">
+					<h2 class="voice3">{{ item.name }}</h2>
+					<div
+						v-if="item.notes"
+						class="itemNotes"
+					>
+						<em>{{ item.notes }}</em>
+					</div>
+					<!-- <p>${{ item.price }}</p> -->
+					<div>${{ item.subtotal }}</div>
 				</div>
-				<!-- <p>${{ item.price }}</p> -->
-				<div>${{ item.subtotal }}</div>
-				<button
-					v-if="item.quantity == 1"
-					@click="cart.remove(item)"
-				>
-					D
-				</button>
-				<button
-					v-if="item.quantity > 1"
-					@click="cart.minusOne(item)"
-				>
-					-
-				</button>
-				{{ item.quantity }}
-				<button @click="cart.plusOne(item)">+</button>
+				<div class="quantity-buttons">
+					<button
+						v-if="item.quantity == 1"
+						@click="cart.remove(item)"
+					>
+						<SvgIcons
+							class="svg-icon"
+							name="close"
+						/>
+					</button>
+					<button
+						v-if="item.quantity > 1"
+						@click="cart.minusOne(item)"
+					>
+						<SvgIcons
+							class="svg-icon"
+							name="subtract"
+						/>
+					</button>
+					<div class="quantity">{{ item.quantity }}</div>
+					<button @click="cart.plusOne(item)">
+						<SvgIcons
+							class="svg-icon"
+							name="add"
+						/>
+					</button>
+				</div>
 			</li>
 		</ul>
 
 		<!-- <p>Total quantity: {{ cart.quantity }}</p> -->
 		<ul class="cartCalc">
-			<li>Cart Subtotal: ${{ cart.allSubtotal }}</li>
+			<li class="voice3">Cart Subtotal: ${{ cart.allSubtotal }}</li>
 			<!-- <li>Delivery fee:</li>
 		<li>Tax:</li>
 		<li>Total:</li> -->
@@ -83,10 +108,118 @@
 		color: var(--light-ink);
 		fill: var(--light-ink);
 	}
-	.cartItem {
-		border: 1px solid red;
+
+	.full-cart {
+		/*		border: 1px solid red;*/
+		display: flex;
+		flex-direction: column;
+		gap: 50px;
+		padding: 20px;
+		overflow: scroll;
 	}
-	em {
+
+	.full-cart .header {
+		display: flex;
+		flex-direction: column;
+		gap: 15px;
+	}
+
+	.voice1 {
+		font-size: 2.5rem;
+	}
+	.full-cart button {
+		background-color: var(--highlight);
+		border: none;
+		border-radius: 10px;
+		color: var(--white);
+		font-family: inherit;
+		font-size: 1.2rem;
+		padding: 10px 20px;
+		box-shadow: var(--shadow-subtle);
+		transition: 0.3s ease-in-out;
+	}
+
+	.full-cart button:hover {
+		background-color: var(--highlight-hover);
+		transition: 0.3s ease-in-out;
+	}
+
+	.clear-contain button {
+		font-size: 1rem;
+		padding: 5px 10px;
+		background-color: var(--warn);
+		transition: 0.3s;
+	}
+
+	.clear-contain button:hover {
+		background-color: var(--warn-hover);
+		transition: 0.3s;
+	}
+
+	.cart-list {
+		/*		border: 1px solid green;*/
+		display: flex;
+		flex-direction: column;
+		gap: 30px;
+	}
+	.cart-item {
+		border: 1px solid var(--light-gray);
+		border-radius: 10px;
+		background-color: var(--paper);
+		padding: 15px;
+		display: flex;
+		flex-direction: row;
+		align-items: center;
+		/*				justify-items: center;*/
+		justify-content: space-between;
+		box-shadow: var(--card-shadow);
+		/*		align-content: center;*/
+	}
+
+	.quantity-buttons {
+		background-color: var(--paper-gray);
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		gap: 0.5rem;
+		border: 1px solid var(--light-gray);
+		border-radius: 15px;
+		overflow: hidden;
+		padding: 5px;
+		box-shadow: var(--shadow-subtle);
+	}
+
+	.quantity-buttons button {
+		padding: 10px 12px;
+		background-color: var(--white);
+		color: var(--white);
+		border-radius: 10px;
+		border: none;
+		transition: 0.3s ease-in-out;
+	}
+
+	.quantity-buttons button:hover {
+		background-color: var(--paper-color);
+		transition: 0.3s ease-in-out;
+	}
+
+	.quantity-buttons svg {
+		width: 15px;
+		height: 15px;
+		fill: var(--highlight);
+	}
+
+	.quantity-buttons .quantity {
+		width: 2rem;
+		text-align: center;
+	}
+
+	img {
+		width: 100%;
+		height: 100%;
+		object-fit: cover;
+	}
+	.cart-item em {
 		font-style: italic;
 	}
 </style>
