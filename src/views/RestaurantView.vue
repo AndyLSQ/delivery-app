4a445070bdfce3148068f089c6e60295f431bb30
Andy <andrew.eliscu@gmail.com>
2023-02-27 23:21:41 -0600

    Restaurant styling

--- src/views/RestaurantView.vue
+++ src/views/RestaurantView.vue
@@ -21,14 +21,69 @@
 	// 	});
 	// });
 
-	const currentRestaurant2 = ref(useDocument(doc(db, 'restaurants', route.params.slug)));
+	const currentRestaurant = ref(useDocument(doc(db, 'restaurants', route.params.slug)));
 </script>
 
 <template>
 	<!-- <div>cr1: {{ currentRestaurant1.name }}</div> -->
-	<div v-if="currentRestaurant2">cr2: {{ currentRestaurant2.name }}</div>
+
+	<div
+		class="banner"
+		v-if="currentRestaurant"
+	>
+		<picture class="banner-image">
+			<img
+				:src="currentRestaurant.imageUrl"
+				alt=""
+			/>
+		</picture>
+		<div class="banner-text">
+			<h2 class="loud-voice title">{{ currentRestaurant.name }}</h2>
+			<div class="restaurant-info voice2">
+				{{ currentRestaurant.priceLevel }} • {{ currentRestaurant.avgWaitTime }}
+			</div>
+		</div>
+		<!-- <p>{{ currentRestaurant.imageUrl }}</p> -->
+	</div>
 	<!-- <div>Tags: {{ currentRestaurant1 }}</div> -->
 	<MenuList :key="$route.path" />
 </template>
 
-<style scoped></style>
+<style scoped>
+	.banner-text {
+		color: var(--white);
+		/*		transform: translate(0, -150px);*/
+		margin-top: -150px;
+		position: relative;
+		z-index: 5;
+		display: flex;
+		flex-direction: column;
+		gap: 1rem;
+		padding: 5px 30px;
+		/*text-shadow: -1px 1px 1px var(--highlight), 1px 1px 1px var(--highlight),
+			1px -1px 0 var(--highlight), -1px -1px 0 var(--highlight);*/
+	}
+
+	.banner-image {
+		height: 450px;
+		left: 50%;
+		margin-top: -30px;
+		margin-left: -50vw;
+		margin-right: -50vw;
+		max-width: 100vw;
+		position: relative;
+		right: 50%;
+		width: 100vw;
+	}
+
+	.banner-image img {
+		width: 100%;
+		height: 100%;
+		object-fit: cover;
+		filter: brightness(40%);
+	}
+
+	.restaurant-info {
+		font-weight: 500;
+	}
+</style>
