a2b6e54f80d282a6f037b4898143b2e095f718ee
Andy <andrew.eliscu@gmail.com>
2023-02-27 18:28:29 -0600

    Set up user carts

--- src/stores/cart.js
+++ src/stores/cart.js
@@ -1,7 +1,7 @@
 import { ref, reactive, computed, watch } from 'vue';
 import { defineStore } from 'pinia';
-import { useFirestore, useCollection } from 'vuefire';
-import { collection, doc, addDoc, deleteDoc, setDoc } from 'firebase/firestore';
+import { useFirestore, useCollection, useDocument } from 'vuefire';
+import { collection, doc, getDoc, addDoc, deleteDoc, setDoc, updateDoc } from 'firebase/firestore';
 import { userService } from '@/services/userService';
 
 export const useCartStore = defineStore('cart', function () {
@@ -26,49 +26,99 @@ export const useCartStore = defineStore('cart', function () {
 	// == Setup
 	// Pull cart DB
 	const db = useFirestore();
-	const cartDb = useCollection(collection(db, 'cart'));
+	const cartDb = useCollection(collection(db, 'carts'));
 	// Set constants - LS cart list
 	const localCartList = ref(window.localStorage.getItem('cartList'));
 
 	// ==== ADD ITEMS TO CART function: ====
-	function findRestaurantCart() {
-		const foundCart = localCartList.value.find(
-			(localCart) => localCart.restaurantId == restaurantId,
-		);
-		console.log('foundCart: ', foundCart);
-		return foundCart;
+	async function findRestaurantCart(currentRestaurantId) {
+		let localCarts = JSON.parse(window.localStorage.getItem('cartList'));
+		let cartData = '';
+		let match = false;
+		console.log('localCarts: ', localCarts);
+		// const dbCartMatchId =
+		await localCarts.forEach(async function (localCartId) {
+			const docSnap = await getDoc(doc(db, 'carts', localCartId));
+			cartData = await docSnap.data();
+			console.log('cartData: ', cartData);
+			// console.log('cartData.restaurantId', cartData.restaurantId);
+			console.log('currentRestaurantId', currentRestaurantId);
+			// return cartData.restaurantId == currentRestaurantId;
+
+			if ((await cartData.restaurantId) == currentRestaurantId) {
+				await console.log('MATCH FOUND');
+				match = true;
+			} else {
+				await console.log('MATCH NOT FOUND');
+			}
+		});
+
+		return match;
+
+		// console.log('dbCartMatchId: ', dbCartMatchId);
+
+		// return dbCartMatchId;
+
+		// createLocalCart(currentRestaurantId);
 	}
 
-	async function createCart(userId, restaurantId) {
-		const newCart = await addDoc(collection(db, 'cart'), {
-			userId,
-			restaurantId,
+	async function createLocalCart(currentRestaurantId) {
+		//Create in FB
+		const newCartRef = await addDoc(collection(db, 'carts'), {
+			userId: 'anonymous',
+			restaurantId: currentRestaurantId,
 			items: [],
 		});
-		console.log('New document written with ID: ', newCart.id);
+		console.log('New cart written with ID: ', newCartRef.id);
+
+		//Add cartId to LS
+		let cartList = JSON.parse(window.localStorage.getItem('cartList'));
+		console.log('cartList: ', cartList);
+		let newCartId = newCartRef.id;
+		// console.log('cartList', cartList);
+		cartList.push(newCartId);
+
+		localStorage.setItem('cartList', JSON.stringify(cartList));
+
+		let newItemList = ['test taco', 'test burrito'];
+
+		updateDoc(doc(db, 'carts', newCartRef.id), { items: newItemList });
+
+		// console.log('newCartRef.items: ', newCartRef.items);
+
+		// if (localCartList.value) {
+		// 	console.log('theres a local cart list: ', localCartList.value);
+
+		// }
+
+		// console.log('newCartList: ', newCartList);
+		// window.localStorage.setItem('cartList', newCartList);
 	}
 
-	function addCartToLs() {}
-
-	function addToCart(restaurantId) {
-		console.log('restaurantId: ', restaurantId);
+	async function addToCart(currentRestaurantId) {
+		console.log('currentRestaurantId: ', currentRestaurantId);
 		// == A. If logged out
 		if (!user.authUser) {
 			// 1. check if any carts in local storage cart list
-			if (localCartList.value) {
-				console.log('yes cart list. ', localCartList.value);
+			if (await window.localStorage.getItem('cartList')) {
+				console.log('yes existing cart list. ', localCartList.value);
 				// 2. loop thru carts in LS, check each for restaurant ID match
-				if (findRestaurantCart()) {
-					console.log('foundCart: ', foundCart);
-					// do something with the foundCart (reduce & add new item to it)
+				if (await findRestaurantCart(currentRestaurantId)) {
+					console.log('FOUND CART MATCH IN DB: ');
+					// console.log('foundCart: ', findRestaurantCart(currentRestaurantId));
+					// 	// do something with the foundCart (reduce & add new item to it)
+				} else {
+					console.log('CART MATCH NOT FOUND IN DB');
 				}
 
 				// 2a. if no match, create a new cart,
 				// add item, & assign cartId to local storage
 			} else {
-				console.log('no cart list. ', localCartList.value);
-				// 3. if no carts in LS, create a new cart
-				createCart('anonymous', restaurantId);
+				console.log('no cart list yet. ', localCartList.value);
+				// 3. if no carts in LS, create a new blank cartlist
+				localStorage.setItem('cartList', JSON.stringify([]));
+				createLocalCart(currentRestaurantId);
+
 				// add item, & assign cartId to local storage
 			}
 		}
@@ -84,7 +134,7 @@ export const useCartStore = defineStore('cart', function () {
 	// == B. If logged in
 	// 1. check user doc for carts
 	// 2. if no carts, skip to 3a
-	// 3. if any, check if restaurantId matches current restaurant
+	// 3. if any, check if currentRestaurantId matches current restaurant
 	// 3a. if not, create new cart, add item
 	// 3b. if so, REDUCE and add item to cart
 	// 4. write cart to db
