df68badb8c128834b6e914a01b8a7223a86b3bbd
Andy <andrew.eliscu@gmail.com>
2023-02-28 16:29:39 -0600

    Pass additional arguments

--- src/components/MenuItemCard.vue
+++ src/components/MenuItemCard.vue
@@ -157,7 +157,7 @@
 						<button
 							@click.prevent="
 								// TODO: USE THESE ARGS: cart.add(cartAddItem, qty, notes);
-								cart.addToCart(route.params.slug);
+								cart.addToCart(item, notes);
 								closeCartAdd();
 							"
 						>
